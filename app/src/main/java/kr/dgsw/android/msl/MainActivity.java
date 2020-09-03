37acfe1b8cbb4e42f11196132fbd7f2437cd1b05
stck622 <stck622@naver.com>
2020-09-03 17:27:26 +0900

    Retrofit, Crashlytics, Firebase, glide Add

---
+++ app/src/main/java/kr/dgsw/android/msl/MainActivity.java
@@ -0,0 +1,20 @@
+package kr.dgsw.android.msl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        throw new RuntimeException("Test Crash"); // Force a crash
+    }
+}
+
